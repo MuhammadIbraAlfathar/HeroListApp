82d2f2fedd2c1e444ed16158c7451a07133be24a
ibraalfthr <108167165+MuhammadIbraAlfathar@users.noreply.github.com>
2023-05-21 18:58:39 +0700

    Update HeroRepository.kt

--- app/src/main/java/com/example/jetheroes/repository/HeroRepository.kt
+++ app/src/main/java/com/example/jetheroes/repository/HeroRepository.kt
@@ -4,8 +4,10 @@ import com.example.jetheroes.model.Hero
 import com.example.jetheroes.model.HeroesData
 
 class HeroRepository {
+    // Get heroes
     fun getHeroes(): List<Hero> = HeroesData.heroes
 
+    // Search heroes
     fun searchHeroes(query: String): List<Hero> {
         return HeroesData.heroes.filter {
             it.name.contains(query, ignoreCase = true)
